7be155e9e5043d718d04b7b79646d8eb78c431ce
korenbezalel <koren8bezalel@gmail.com>
2024-04-02 14:49:52 +0300

    מעבר לשרת חיצוני והוספת פיצ'ר סידור רנדומאלי
    חלק זה מתמקד המעבר לשרת ציבורי, כלומר שיהיה ניתן להתחבר עליו מחוץ למכונה ומחוץ לרשת הביתית.
    בנוסך הוספתי פיצ'ר המקבל את התור שנוצר באיטרציה הקודמת ומחלק את חמשת השחקנים הראשונים ל2 קבוצות ע"פ חוקי המשחק שפורטו במסמכים

--- server.py
+++ server.py
@@ -1,34 +1,53 @@
 import socket
 import threading
 import queue
+from random import randint
 #global variables
-dict = {}
+clients_dict = {}
 
-#entry argument: this function gets server_socket type socket and q which is enmpty for now
-#closing argument: this function hundle the connection between the client said and the server side
+#entry argument: this function gets server_socket type socket and q which is empty for now
+#closing argument: this function handle the connection between the client said and the server side
 def client_handler(server_socket, q):
     (client_socket, client_addres) = server_socket.accept()
-    dict[client_socket] = client_addres
+    print('client made a connection')
+    clients_dict[client_socket] = client_addres
     q.put(client_socket)
 
 #this function aims to run the client_handler function 5 times in 5 different threads
-def connection_hendler():
+def connection_handler(server_socket, q):
     threads = []
     for _ in range(5):
-        t = threading.Thread(target=client_handler)
+        t = threading.Thread(target=client_handler, args= (server_socket,q))
         t.start()
         threads.append(t)
     for i in threads:
         i.join()
 
-#main function axicute the code above
+def Random_divader(q):
+    n = randint(0,5)
+    i = 5
+    groupI = []
+    groupII = []
+    for _ in range(n):
+        proupII.append(q.get())
+        i-=1
+    proupI.append(q.get())
+    for _ in range(i):
+        groupII.append(q.get())
+    return (groupI, groupII)
+
+
+
+#main function exciute the code above
 def main():
+
     q = queue.Queue()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(("127.0.0.1", 50658))
+    server_socket.bind(("192.168.31.11", 50658))
     server_socket.listen()
     print("server in up and running")
-    connection_hendler()
+    connection_handler(server_socket, q)
+    (G1,G2) = Random_divader(q)
 
 
 if __name__ == '__main__':
